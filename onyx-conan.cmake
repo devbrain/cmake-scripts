5717b68aebee60f5e4f8e499b492f9c34fba3f85
Igor <igor.gutnik@gmail.com>
2020-05-06 22:47:22 +0300

    initial release

---
+++ onyx-conan.cmake
@@ -0,0 +1,29 @@
+macro(onyx_setup_conan)
+  if(CONAN_EXPORTED) # in conan local cache
+    set (CONANBUILDINFO_FILE "${CMAKE_BINARY_DIR}/../conanbuildinfo.cmake")
+    include(${CONANBUILDINFO_FILE})
+  else()
+    if(NOT EXISTS "${CMAKE_SOURCE_DIR}/cmake/conan/conan.cmake")
+      if (NOT EXISTS ${CMAKE_BINARY_DIR}/conan.cmake)
+	message(STATUS "Downloading conan.cmake from https://github.com/memsharded/cmake-conan")
+	file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/master/conan.cmake"
+	  "${CMAKE_BINARY_DIR}/conan.cmake"
+	  SHOW_PROGRESS STATUS status)
+	message(STATUS "Download status = ${status}")
+      else ()
+	message (STATUS "Using existing ${CMAKE_BINARY_DIR}/conan.cmake")
+      endif ()
+      include(${CMAKE_BINARY_DIR}/conan.cmake)
+    else()
+      include(${CMAKE_SOURCE_DIR}/cmake/conan/conan.cmake)
+    endif()
+    
+    conan_cmake_run(
+      ${ARGN}
+      GENERATORS cmake_find_package
+      BUILD missing
+      BASIC_SETUP
+      )
+    conan_load_buildinfo ()
+  endif()
+endmacro()
